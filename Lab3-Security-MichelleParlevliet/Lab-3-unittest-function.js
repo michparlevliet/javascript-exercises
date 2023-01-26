e914dfc9c1237053b3d90c81032940086f52f6a1
Michelle Parlevliet <113198354+michparlevliet@users.noreply.github.com>
2023-01-25 19:43:27 -0500

    unit testing lab

---
+++ Lab3-Security-MichelleParlevliet/Lab-3-unittest-function.js
@@ -0,0 +1,28 @@
+//LAB 3 - UNIT TESTING
+//======== THIS FILE IS FOR THE checkHumbrId FUNCTION ==========
+
+/** checkHumbrId Function
+ * Validate input as a Humber College Student number.
+ * Returns true if input validates.
+ * @param {string} idIn
+ */
+// var humbrId = /[n|N]\d{8}/;
+function checkHumbrId(idIn) {
+  "use strict";
+  let humbrId = /^(n|N)\d{8}$/;
+  let isIdValid = false;
+
+  console.log(humbrId.test(idIn));
+
+  let check = humbrId.test(idIn);
+
+  if (check == true) {
+    isIdValid = "==PASSED==";
+  }
+ 
+  else {
+    isIdValid = "xxFAILEDxx";
+  }
+
+  return isIdValid;
+}// end checkHumbrId
